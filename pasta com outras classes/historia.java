e4cce1f8e1170c896873c2a34701a494cc4339bc
KelvinVargasM <159589673+KelvinVargasM@users.noreply.github.com>
2024-05-07 10:33:30 -0300

    Add files via upload

---
+++ pasta com outras classes/historia.java
@@ -0,0 +1,35 @@
+package estudo;
+
+public class historia {
+	public static void main(String[] args) {
+		System.out.println("Em uma casa no meio da floresta, você é acordado com uma sensação de inquitação");
+		System.out.println("Então decide vasculhar a casa... acaba se assustando ao descobrir que o mundo ao seu redor mudou drasticamente");
+		System.out.println("Vultos e sons perturbadores preenchem o ambiente...");
+		System.out.println("Ao olhar pela janela, se depara com um cenário estranho e surreal, completamente diferente do que estava habituado.");
+		System.out.println();
+		System.out.println("Então decide sair da casa, para encontrar uma maneira de sair desse pesadelo...");
+		System.out.println("Depois de sair de casa e ver que tudo está diferente ele fica com dúvida para onde ir");
+		System.out.println("op1- Seguir a estrela mais brilhante");
+		System.out.println("op2- Subir a maior montanha que ele avista");
+		System.out.println();
+		System.out.println("1- Enquanto seguia a estrela acaba se deparando com um inimigo, uma Aranha gigante nivel 2");
+		System.out.println("2- Quando chegava perto da montanha acaba encontrando alguns Esqueletos nivel 1");
+		System.out.println();
+		System.out.println("1- Ao eliminar a Aranha gigante manteve seu caminho até chegar a uma caverna, mais para descer presisara escorregar pelas vinhas tendo a chance de se machucar");
+		System.out.println("2- Depois de eliminar os Esqueletos continuou a subir a montanha, agora precisara escalar para conseguir uma recompensa");//40% de chance de não subir a montanha
+	    System.out.println();
+	    System.out.println("1- Depois de descer a caverna encontra Goblis com adagas nivel 1");// recompensa aumenta o dano e recupera vida
+	    System.out.println("2- ");
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
